fbee83515f1780e08174b9317c448fe7d7b67746
TMBelkina <dr.belkina.1997@gmail.com>
2022-09-12 15:50:00 +0300

    first commit

---
+++ homework/hw_12_poddubnaya_unready/styles/js/index.js
@@ -0,0 +1,19 @@
+let a = prompt('Введите фамилию');
+console.log(`Ваша фамилия: ${a}`);
+
+let b = prompt('Введите имя');
+console.log(`Ваше имя: ${b}`);
+
+let c = prompt('Введите отчество');
+console.log(`Ваше отчество: ${c}`);
+
+let d = prompt('Введите Ваш возраст');
+console.log(`Ваш возраст: ${d}`);
+
+let e = confirm('Ваш пол мужской?');
+if (true){
+    console.log('Ваш пол: мужской');
+} else {
+    console.log('Ваш пол: женский');
+}
+
